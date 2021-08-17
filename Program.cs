da46ff963ae0573203c5c94007a1db6a8e263220
Drew Wilson <jankyy93@gmail.com>
2021-08-17 11:19:49 -0400

    Added ConsoleTables package

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseBodyAsAStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=drew-wilson");
+
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseBodyAsAStream);
+
+        }
     }
-  }
 }
---
+++ item.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace OneListClient
+{
+    public class Item
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("complete")]
+        public bool Complete { get; set; }
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        public string CompletedStatus
+        {
+            get
+            {
+                return Complete ? "Completed" : "Not Completed";
+            }
+        }
+    }
+}
